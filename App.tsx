216f3030df77bcf36823633436bdb683494a6473
irassari <elassari19@gmail.com>
2021-11-27 19:43:23 +0100

    check if the git repo is correct

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Camera from './src';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+      <Camera />
+    </SafeAreaView>
   );
 }
 

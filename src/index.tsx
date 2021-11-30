53ba6c7c44ec1dbbc6b53642ef162a9e7750bc68
irassari <elassari19@gmail.com>
2021-11-30 17:54:58 +0100

    added the image picker functions

--- src/index.tsx
+++ src/index.tsx
@@ -2,25 +2,51 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Image, Pressable } from 'react-native';
 import { Camera } from 'expo-camera';
 import { CameraType, FlashMode } from 'expo-camera/build/Camera.types';
-// import * as ImagePicker from 'expo-image-picker';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App(): JSX.Element {
 
-  const [camera, setCamera] = useState<any>();
+  const [cameraPermission, setCameraPermission] = useState<any>();
+  const [galleryPermission, setGalleryPermission] = useState<any>();
   const [type, setType] = useState <CameraType>(Camera.Constants.Type.back);
   const [flash, setFlash] = useState<FlashMode>(Camera.Constants.FlashMode.auto);
   const [photo, setPhoto] = useState<string>('');
-console.log(camera);
+  const [image, setImage] = useState<string>('');
+
   useEffect(() => {
-    (async () => {
+    (async (): Promise<void> => {
+
+      // check permission of camera
       const {status} = await Camera.requestCameraPermissionsAsync() ;
-      setCamera(status === 'granted');
+      setCameraPermission(status === 'granted');
+
+      // check the permission of oper gallery
+      const galleryStatus = (await ImagePicker.requestMediaLibraryPermissionsAsync()).status;
+      if (galleryStatus !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }else{setGalleryPermission(galleryStatus !== 'granted')}
+
     })();
+
   }, []);
+
+  // function for handle the image picker
+  const pickImage = async (): Promise<void> => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
 
   // function to take the pictures
   const pictures = async():Promise<void> =>{
-    let photo = camera && await camera.takePictureAsync();
+    let photo = cameraPermission && await cameraPermission.takePictureAsync();
     setPhoto(photo.uri);
   }
 
@@ -35,19 +61,19 @@ console.log(camera);
   }
 
   // no camera on device
-  if (camera === null) {
+  if (cameraPermission === null) {
     return <View />;
   }
 
   // don't have permission
-  if (camera === false) {
+  if (cameraPermission === false || galleryPermission === false) {
     return <Text>No access to camera</Text>;
   }
 
   return (
     <View style={styles.container}>
       <Camera style={styles.camera} 
-        ref={ref=>setCamera(ref)}
+        ref={ref=>setCameraPermission(ref)}
         type={type} flashMode={Camera.Constants.FlashMode.off}
         autoFocus={Camera.Constants.AutoFocus.on}
         ratio={'16:9'}
@@ -90,6 +116,11 @@ console.log(camera);
 
         </View>
       </Camera>
+      image picker
+      <View>
+        <Pressable onPress={pickImage}>Pick image or video from gallery</Pressable>
+        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      </View>
       <View style={{flex: 1}}>
         {
           photo ? <Image style={{flex:1}} source={{uri: photo}} />
